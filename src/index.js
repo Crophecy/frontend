c78da77c8dc3d36646e63522085ad78478c1d179
Abhijeet Pundkar <98893864+AbhiPundkar@users.noreply.github.com>
2024-03-07 10:22:50 +0530

    added about_page/contactForm

--- src/index.js
+++ src/index.js
@@ -12,13 +12,18 @@ import Home from './views/home'
 import NotFound from './views/not-found'
 import Log from './views/log'
 
+import About from './views/about'
+
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route component={Home} exact path="/" />
         <Route component={Log} exact path="/log" />
+        <Route component={About} path="/about" />
+        
         <Route component={NotFound} path="*" />
+        
       </Switch>
     </Router>
   )
